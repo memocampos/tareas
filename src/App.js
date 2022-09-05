62e03959c4a6cea5e52f8c50e9137511237160f2
camposguillermo <camposguillermo@hotmail.com>
2022-09-05 17:55:28 -0500

    code

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import logo from './logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className='logo-contenedor'>
+          <img 
+            src={logo}
+            className='logo'
+            alt="logo" 
+        
+          /> 
+      </div>
+      
+      <div className='tareas-lista-principal'>
+          <h1>Mis Tareas</h1>
+          <ListaDeTareas />
+
+      </div>
+        
+    </div>
+  );
+}
+
+export default App;
